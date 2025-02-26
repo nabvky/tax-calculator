dcec7337ef15ce90470fa679f2dfe87d74e2d549
nanvky <vikas.yadav@nabard.org>
2025-02-26 13:09:19 +0530

    added tax calculator

---
+++ app/tax-calculator/page.js
@@ -0,0 +1,10 @@
+import TaxCalculator from "@/components/TaxCalculator";
+
+export default function TaxCalculatorPage() {
+    return (
+        <div style={{ width: "100%", maxWidth: "1200px", margin: "auto", padding: "16px" }}>
+        <TaxCalculator/>
+      </div>
+    );
+  }
+  
